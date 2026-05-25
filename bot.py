3eee0ac72e8eb8b8548d4d4a17b356e230092b7b
wwwnikitoa1994-afk <www.nikitoa1994@gmail.com>
2026-05-25 15:28:49 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import asyncio
+
 from aiogram import Bot, Dispatcher, F
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
@@ -10,7 +12,13 @@ from aiogram.types import Message
 TOKEN = "8634367728:AAG_gKuluoogGD2km02bakEH35kjvr6nALU"
 CHANNEL_ID = "@khv_marketplace"
 
-bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(
+    token=TOKEN,
+    default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML
+    )
+)
+
 dp = Dispatcher(storage=MemoryStorage())
 
 
