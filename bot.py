c2a4bf84408d2fd7ada557765f2bff012e9bf9e1
wwwnikitoa1994-afk <www.nikitoa1994@gmail.com>
2026-05-26 09:38:02 +0400

    bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ from aiogram.types import (
 # CONFIG
 # =========================================
 
-TOKEN = "8634367728:AAG_gKuluoogGD2km02bakEH35kjvr6nALU"
+TOKEN = "8634367728:AAEhXbeRhk30alaxl5h4GV_ GWscp8YJGTKU:AAG_gKuluoogGD2km02bakEH35kjvr6nALU"
 CHANNEL_ID = "@khv_marketplace"
 
 BOT_LINK = "https://t.me/khv_marketplace_bot"
